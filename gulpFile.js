3a4e27c1622bb632208340230335ab8a05b89fee
AndrewZamora <andrewmichaelzamora@gmail.com>
2017-12-01 14:35:21 +0900

    Revised Gulp File

--- gulpFile.js
+++ gulpFile.js
@@ -4,7 +4,7 @@ const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
 // Compile Sass
-gulp.task('sass', function(){
+gulp.task('sass', function () {
   return gulp.src(['src/scss/*.scss'])
     .pipe(sass())
     .pipe(autoprefixer(['last 2 versions']))
@@ -13,11 +13,10 @@ gulp.task('sass', function(){
 });
 
 // Watch and Serve
-gulp.task('serve', ['sass'], function(){
+gulp.task('serve', ['sass'], function () {
   browserSync.init({
     server: './src'
   });
-
   gulp.watch(['src/scss/*.scss'], ['sass']);
   gulp.watch(['src/*.html']).on('change', browserSync.reload);
 });
