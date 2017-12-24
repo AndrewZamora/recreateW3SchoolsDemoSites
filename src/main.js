fa0d1963c97902062781ddaaf80bc4e587713a88
AndrewZamora <andrewmichaelzamora@gmail.com>
2017-12-24 19:16:50 +0900

    Styled the showcase spans so that some text is displayed none when in mobile mode

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 var ul = document.querySelector("ul");
 var menuBtn = document.getElementById("menuBtn");
+var menuIsOpen = false;
 // Element Variables
 
-menuBtn.addEventListener("click", function(){
-    ul.style.display = "none";
+menuBtn.addEventListener("click", function () {
+    if (menuIsOpen) {
+        ul.style.display = "none";
+        menuIsOpen = false;
+    } else {
+        ul.style.display = "flex";
+        menuIsOpen = true;
+    }
 });
 //Closes Mobile Menu
