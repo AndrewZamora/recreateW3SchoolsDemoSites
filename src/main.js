240fc9baa7c79e9d92e3a44adc5174acc5ace8ad
AndrewZamora <andrewmichaelzamora@gmail.com>
2018-01-12 14:30:51 +0900

    Added toggle support for  IE9

--- src/main.js
+++ src/main.js
@@ -4,6 +4,19 @@ var menu = document.getElementById("menu");
 
 // Open or Closes Menu
 menuButton.addEventListener("click", menuOpenClose);
+
 function menuOpenClose() {
-    menu.classList.toggle("menuOpen");
- }
+    if (menu.classList) {
+        menu.classList.toggle("menuOpen");
+    } else {
+        // For IE9 Support
+        var classes = menu.className.split(" ");
+        var i = classes.indexOf("menuOpen");
+
+        if (i >= 0)
+            classes.splice(i, 1);
+        else
+            classes.push("menuOpen");
+        menu.className = classes.join(" ");
+    }
+}
